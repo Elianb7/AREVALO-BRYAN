ccb6c87ebf3c9a6bb6d56d449588b2660c4762e4
KevinChamorro <60003927+kevinchamorro33@users.noreply.github.com>
2020-03-10 11:55:13 -0500

    trabajos de clases

--- JavaApplication16/src/javaapplication16/JavaApplication16.java
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -16,19 +16,31 @@ public class JavaApplication16 {
      */
     
     //Entero -32768 a 32767
+   
     int numeroEntero=0;
     double numeroDouble=0.0;
     char letras='A';
     String cadena="Hola mundo cruel de corino";
     boolean valorBool=true;
-    valorBool=true
-    boolean valorBool=1;
-    
-    
+    //valorBool=true;
+    //boolean valorBool=1;
+     public static void imprirNombre(){
+     System.out.println("Elian");}
+    public static int sumae2Numeros(){
+    return 0;
+       } 
+        public static int sumae2Numeros(int num1, int num2){
+        int suma=num1+num2;
+        return suma;
+        }
   
     public static void main(String[] args) {
         // TODO code application logic here
+        imprirNombre ();
+        sumae2Numeros(1,5);
+        System.out.println(sumae2Numeros(1,5));
     }
+    
     //comentario de una linea.
     
     /*
