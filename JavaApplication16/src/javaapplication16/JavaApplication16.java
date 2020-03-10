eec6756d052b5a0a74a7d300735d010a2865edb5
KevinChamorro <60003927+kevinchamorro33@users.noreply.github.com>
2020-03-10 11:07:08 -0500

    tipo de dato

---
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author Sistemas
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Entero -32768 a 32767
+    int numeroEntero=0;
+    double numeroDouble=0.0;
+    char letras='A';
+    String cadena="Hola mundo cruel de corino";
+    boolean valorBool=true;
+    valorBool=true
+    boolean valorBool=1;
+    
+    
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    //comentario de una linea.
+    
+    /*
+    
+    */
+    
+    
+}
